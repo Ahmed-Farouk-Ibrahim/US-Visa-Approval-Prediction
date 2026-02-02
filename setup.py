b5d5079a7ee934ab9960c4aaf3bbb4c280289c32
Ahmed-Osman-AI <engineer.ahmedfarouk@gmail.com>
2026-02-02 14:55:38 -0400

    Adding requirements.txt & setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="us_visa_approval_prediction",
+    version="0.0.1",
+    author='Ahmed Osman',
+    author_email='engineer.ahmedfarouk@gmail.com',
+    packages=find_packages()
+)
